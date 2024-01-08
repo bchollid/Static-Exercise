864f663d66d0a934e98e1b1f9f5d1fda1a8fc588
bchollid <brandonholliday1@gmail.com>
2024-01-08 09:55:48 -0700

    Finished

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Welcome to the temperature converter!");
+            Console.WriteLine("Press 1 to convert F to C. Press 2 to convert C to F.");
+
+            var userChoice = Console.ReadLine();
+            if(userChoice == "1")
+            {
+                Console.WriteLine("We'll convert F to C.");
+                Console.WriteLine("Enter the temperature in fahrenheit you wish to convert.");
+                decimal temp = decimal.Parse(Console.ReadLine());
+                Console.WriteLine(TempConverter.FahrenheitToCelsius(temp));
+         
+            }
+            else if (userChoice == "2")
+            {
+                Console.WriteLine("We'll convert C to F.");
+                Console.WriteLine("Enter the temperature in celsius you wish to convert.");
+                decimal temp = decimal.Parse(Console.ReadLine());
+                Console.WriteLine(TempConverter.CelsiusToFahrenheit(temp));
+            }
+            else
+            {
+                Console.WriteLine("Please choose either 1 or 2.");
+            }
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+
+        public static decimal FahrenheitToCelsius(decimal temp)
+        {
+            decimal conversion;
+            conversion = (temp - 32) * 5 / 9;
+            return conversion;
+        }
+        public static decimal CelsiusToFahrenheit(decimal temp)
+        {
+            //(0°C × 9 / 5) +32 = 32°F
+            decimal conversion;
+            conversion = (temp * 9 / 5) + 32;
+            return conversion;
+
+        }
+    }
+}
+
